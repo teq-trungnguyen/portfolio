e42718cca6fce072dbc1af275954ffc617eaec7b
teq-trungnguyen <trungnguyen@teqnological.asia>
2026-04-14 16:53:53 +0700

    chore: update production basePath and assetPrefix to /portfolio

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === "production" ? "/nextjs-portfolio" : "",
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "/nextjs-portfolio/" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/portfolio/" : "",
   images: {
     unoptimized: true,
   },
   env: {
     NEXT_PUBLIC_BASE_PATH:
-      process.env.NODE_ENV === "production" ? "/nextjs-portfolio" : "",
+      process.env.NODE_ENV === "production" ? "portfolio" : "",
   },
 };
 
