64bf9d4008b8f3f811fe99783c25ec5ba4a76425
teq-trungnguyen <trungnguyen@teqnological.asia>
2026-04-14 16:06:59 +0700

    chore: remove basePath and assetPrefix configuration for production export

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === "production" ? "/nextjs-portfolio" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/nextjs-portfolio/" : "",
 };
 
 export default nextConfig;
